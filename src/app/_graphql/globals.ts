440b82d096d68f076a3674ad0eacf5f676ffa288
vucd1 <64571700+vucd1@users.noreply.github.com>
2024-04-20 15:56:18 -0700

    modified footer

--- src/app/_graphql/globals.ts
+++ src/app/_graphql/globals.ts
@@ -1,3 +1,11 @@
+// This file typically contains configurations for global components like headers, footers, 
+// navigation menus, or any other data that should be consistent throughout the site.
+
+// The purpose of having a dedicated global.ts file is to centralize the management of this 
+// global data, making it easier to update and maintain. By separating global data from 
+// individual page or component-specific data, it promotes code organization and reusability.
+
+
 import { LINK_FIELDS } from './link'
 
 export const HEADER = `
@@ -16,6 +24,7 @@ query Header {
 
 export const FOOTER = `
   Footer {
+    copyright
     navItems {
       link ${LINK_FIELDS({ disableAppearance: true })}
 		}
--- src/app/_graphql/link.ts
+++ src/app/_graphql/link.ts
@@ -1,14 +1,23 @@
+// interface keyword is a type checker. The properties the interface requires are present and 
+// have the required type. Itdoes not require that these properties come in any sort of order.
+
 interface Args {
   disableLabel?: true
   disableAppearance?: true
 }
 
+// = {} declares a default argument
+// A normal default arg: function name(parameter1=defaultValue1,...) {
+// We're just adding destructuring to this...
 export const LINK_FIELDS = ({ disableAppearance, disableLabel }: Args = {}): string => `{
   ${!disableLabel ? 'label' : ''}
   ${!disableAppearance ? 'appearance' : ''}
   type
   newTab
   url
+  icon {
+    url
+  }
   reference {
     relationTo
     value {
