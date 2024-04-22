f627ae3e2d29530bbc5c7bfe165ea84b630baf6f
vucd1 <64571700+vucd1@users.noreply.github.com>
2024-04-22 13:51:48 -0700

    Revert "modified recover password"
    This reverts commit 2dfa74a61680edb456c8c5d445c271a82ced796c.

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -6,47 +6,12 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
-import Link from 'next/link'
-import Image from 'next/image'
-import { RenderParams } from '../../_components/RenderParams'
 
 export default async function RecoverPassword() {
   return (
-    <section className={classes.recoverPassword}>
-      <div className={classes.heroImg}>
-        <Link href='/'>
-          <Image 
-            src='/logo-black.svg'
-            alt='logo'
-            width={250}
-            height={23}
-            className={classes.logo}
-          />
-        </Link>
-      </div>
-
-     <div className={classes.formWrapper}>
-        <div className={classes.formContainer}>
-          {/* RenderParams is needed to properly route through URL params */}
-          <RenderParams className={classes.params} />
-
-          <Link href='login' className={classes.backLink}>
-            <Image 
-              src='/assets/icons/arrow-left.svg'
-              alt='left arrow'
-              width={24}
-              height={24}
-            />
-            <p>Back</p>
-          </Link>
-          <div className={classes.formTitle}>
-            <h3>Forgot Password</h3>
-          </div>
-          
-          <RecoverPasswordForm />
-        </div>
-      </div>
-    </section>
+    <Gutter className={classes.recoverPassword}>
+      <RecoverPasswordForm />
+    </Gutter>
   )
 }
 
